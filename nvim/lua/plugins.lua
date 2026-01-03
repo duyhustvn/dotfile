333d6fbf667cf0cfe40265e6fb102fba95ba799c
duyhustvn <duy.dinhle.hust.vn@gmail.com>
2026-01-03 14:31:01 +0700

    formatter with conform

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,93 +1,120 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    "tanvirtin/monokai.nvim",
+	"tanvirtin/monokai.nvim",
 	-- LSP manager
 	"williamboman/mason.nvim",
 	"williamboman/mason-lspconfig.nvim",
 	"neovim/nvim-lspconfig",
 
-    -- File explorer
-    {
-      "nvim-tree/nvim-tree.lua",
-      dependencies = {
-        "nvim-tree/nvim-web-devicons", -- file icon
-      },
-    },
+	-- File explorer
+	{
+		"nvim-tree/nvim-tree.lua",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons", -- file icon
+		},
+	},
 
-    -- Git
-    {
-      "NeogitOrg/neogit",
-      dependencies = {
-        "nvim-lua/plenary.nvim",         -- required
-        "sindrets/diffview.nvim",        -- optional - Diff integration
-        -- Only one of these is needed.
-        "nvim-telescope/telescope.nvim", -- optional - For better picker experience
-        "ibhagwan/fzf-lua",              -- optional
-        "echasnovski/mini.pick",         -- optional
-      },
-    },
+	-- Git
+	{
+		"NeogitOrg/neogit",
+		dependencies = {
+			"nvim-lua/plenary.nvim", -- required
+			"sindrets/diffview.nvim", -- optional - Diff integration
+			-- Only one of these is needed.
+			"nvim-telescope/telescope.nvim", -- optional - For better picker experience
+			"ibhagwan/fzf-lua", -- optional
+			"echasnovski/mini.pick", -- optional
+		},
+	},
 
-    -- Find file
-    {
-        'nvim-telescope/telescope.nvim',
-        tag = '0.1.8',
-        dependencies = {
-            'nvim-lua/plenary.nvim',
-            -- Optional: for better sorting performance
-            { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
-        },
-    },
+	-- Find file
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			-- Optional: for better sorting performance
+			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		},
+	},
 
-    -- Autocompletion
-    {
-        "hrsh7th/nvim-cmp",
-        dependencies = {
-            "hrsh7th/cmp-nvim-lsp",           -- LSP completion source
-            "hrsh7th/cmp-buffer",             -- Buffer completion source
-            "hrsh7th/cmp-path",               -- Path completion source
-            "hrsh7th/cmp-cmdline",            -- Cmdline completion source
-            "L3MON4D3/LuaSnip",              -- Snippet engine
-            "saadparwaiz1/cmp_luasnip",      -- Snippet completion source
-            "rafamadriz/friendly-snippets",   -- Useful snippets
-        },
-    },
+	-- Autocompletion
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp", -- LSP completion source
+			"hrsh7th/cmp-buffer", -- Buffer completion source
+			"hrsh7th/cmp-path", -- Path completion source
+			"hrsh7th/cmp-cmdline", -- Cmdline completion source
+			"L3MON4D3/LuaSnip", -- Snippet engine
+			"saadparwaiz1/cmp_luasnip", -- Snippet completion source
+			"rafamadriz/friendly-snippets", -- Useful snippets
+		},
+	},
 
-    { 
-      "nvim-tree/nvim-web-devicons", 
-      opts = {},
-    },
+	{
+		"nvim-tree/nvim-web-devicons",
+		opts = {},
+	},
 
-    {
-        "rust-lang/rust.vim",
-        ft = "rust",
-        init = function ()
-          vim.g.rustfmt_autosave = 1
-        end
-    },
+	{
+		"rust-lang/rust.vim",
+		ft = "rust",
+		-- init = function ()
+		--   vim.g.rustfmt_autosave = 1
+		-- end
+	},
 
-    {
-      "dart-lang/dart-vim-plugin",
-      ft = "dart",
-      init = function()
-        vim.g.dart_format_on_save = 1
-      end,
-    },
+	{
+		"dart-lang/dart-vim-plugin",
+		ft = "dart",
+		-- init = function()
+		--   vim.g.dart_format_on_save = 1
+		-- end,
+	},
 
-    {
-        "nvim-flutter/flutter-tools.nvim",
-    },
+	{
+		"nvim-flutter/flutter-tools.nvim",
+	},
+
+	-- Conform
+	{
+		"stevearc/conform.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			require("conform").setup({
+				formatters_by_ft = {
+					-- Lua
+					lua = { "stylua" },
+					-- C/C++
+					c = { "clang-format" },
+					cpp = { "clang-format" },
+					-- Rust
+					rust = { "rustfmt" },
+					-- Dart
+					dart = { "dart_format" },
+				},
+
+				-- This controls the Format on Save behavior
+				format_on_save = {
+					timeout_ms = 500,
+					lsp_fallback = true, -- Use LSP if the formatter tool isn't found
+				},
+			})
+		end,
+	},
 })
