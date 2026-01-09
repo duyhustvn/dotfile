d869d56b1b4022f535deec017d85d9dc886e99a9
duyhustvn <duy.dinhle.hust.vn@gmail.com>
2026-01-09 08:21:01 +0700

    uninstall rust-analyzer to avoid conflict

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,59 +1,58 @@
-require('mason').setup({
-    ui = {
-        icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗"
-        }
-    }
+require("mason").setup({
+	ui = {
+		icons = {
+			package_installed = "✓",
+			package_pending = "➜",
+			package_uninstalled = "✗",
+		},
+	},
 })
 
-require('mason-lspconfig').setup({
-    -- A list of servers to automatically install if they're not already installed
-    ensure_installed = { 'gopls', 'lua_ls', 'rust_analyzer', 'clangd', 'yamlls', 'ts_ls' },
+require("mason-lspconfig").setup({
+	-- A list of servers to automatically install if they're not already installed
+	ensure_installed = { "gopls", "lua_ls", "clangd", "yamlls", "ts_ls" },
 })
-
 
 -- Set different settings for different languages' LSP
 -- LSP list: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
 -- How to use setup({}): https://github.com/neovim/nvim-lspconfig/wiki/Understanding-setup-%7B%7D
 --     - the settings table is sent to the LSP
 --     - on_attach: a lua callback function to run after LSP attaches to a given buffer
-local lspconfig = require('lspconfig')
+local lspconfig = require("lspconfig")
 
 -- Customized on_attach function
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 local opts = { noremap = true, silent = true }
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+vim.keymap.set("n", "<space>e", vim.diagnostic.open_float, opts)
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist, opts)
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
-    -- Enable completion triggered by <c-x><c-o>
-    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+	-- Enable completion triggered by <c-x><c-o>
+	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    local bufopts = { noremap = true, silent = true, buffer = bufnr }
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-    vim.keymap.set('n', '<space>wl', function()
-        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, bufopts)
-    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-    vim.keymap.set('n', '<space>cr', vim.lsp.buf.rename, bufopts)
-    vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-    vim.keymap.set("n", "<space>f", function()
-        vim.lsp.buf.format({ async = true })
-    end, bufopts)
+	-- See `:help vim.lsp.*` for documentation on any of the below functions
+	local bufopts = { noremap = true, silent = true, buffer = bufnr }
+	vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+	vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+	vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+	vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
+	vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, bufopts)
+	vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
+	vim.keymap.set("n", "<space>wl", function()
+		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+	end, bufopts)
+	vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, bufopts)
+	vim.keymap.set("n", "<space>cr", vim.lsp.buf.rename, bufopts)
+	vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, bufopts)
+	vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
+	vim.keymap.set("n", "<space>f", function()
+		vim.lsp.buf.format({ async = true })
+	end, bufopts)
 end
 
 -- Configure each language
@@ -74,29 +73,30 @@ lspconfig.clangd.setup({
 
 -- Configure YAML LSP
 lspconfig.yamlls.setup({
-    on_attach = on_attach,
-    settings = {
-        yaml = {
-            schemas = {
-                ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
-                ["https://json.schemastore.org/github-action.json"] = "/.github/actions/*/action.yml",
-                ["https://json.schemastore.org/ansible-playbook.json"] = "*playbook*.yml",
-                ["https://json.schemastore.org/kustomization.json"] = "kustomization.yaml",
-                ["https://json.schemastore.org/chart.json"] = "Chart.yaml",
-                ["https://json.schemastore.org/docker-compose.json"] = "docker-compose*.yml",
-                ["https://json.schemastore.org/helmfile.json"] = "helmfile.yaml",
-                ["https://json.schemastore.org/gitlab-ci.json"] = ".gitlab-ci.yml",
-            },
-            format = {
-                enable = true,
-            },
-            validate = true,
-            completion = true,
-            hover = true,
-        },
-    },
+	on_attach = on_attach,
+	settings = {
+		yaml = {
+			schemas = {
+				["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
+				["https://json.schemastore.org/github-action.json"] = "/.github/actions/*/action.yml",
+				["https://json.schemastore.org/ansible-playbook.json"] = "*playbook*.yml",
+				["https://json.schemastore.org/kustomization.json"] = "kustomization.yaml",
+				["https://json.schemastore.org/chart.json"] = "Chart.yaml",
+				["https://json.schemastore.org/docker-compose.json"] = "docker-compose*.yml",
+				["https://json.schemastore.org/helmfile.json"] = "helmfile.yaml",
+				["https://json.schemastore.org/gitlab-ci.json"] = ".gitlab-ci.yml",
+			},
+			format = {
+				enable = true,
+			},
+			validate = true,
+			completion = true,
+			hover = true,
+		},
+	},
 })
 
 lspconfig.ts_ls.setup({
 	on_attach = on_attach,
 })
+
